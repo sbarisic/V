6aad5c138088594b2503c034a5d7d64a24dd00b6
Saša Barišić <cartman300@net.hr>
2015-11-19 00:11:42 +0100

    Renamed most stuff

--- V/Properties/AssemblyInfo.cs
+++ V/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Vermin")]
+[assembly: AssemblyTitle("V Language")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Vermin")]
+[assembly: AssemblyProduct("V")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
